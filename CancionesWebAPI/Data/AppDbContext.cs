3ca645f1d03066e313833b49f011a0ede54ffae9
ValeriaGuty <a2020112787@estudiantes.upsa.edu.bo>
2021-10-13 10:30:41 -0400

    AppDBContext creado

---
+++ CancionesWebAPI/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using CancionesWebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CancionesWebAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base (options)
+        {
+            
+        }
+        public DbSet<Cancion> Cancion { get; set; }
+    }
+}
